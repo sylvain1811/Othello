c08f800b5d1527a97fe96236ebf0c3bb0ca282e8
Sylvain Renaud <sylvain.renaud@he-arc.ch>
2018-01-11 23:53:48 +0100

    Add TreeNode class.

--- OthelloIA_G3/Board.cs
+++ OthelloIA_G3/Board.cs
@@ -9,10 +9,12 @@ namespace OthelloIA_G3
         // ATTRIBUTS
         private int[,] board;
         private static string NAME = "Costa_Renaud";
+        private int color;
 
         // CONSTRUCTEUR
-        public Board()
+        public Board(int color)
         {
+            this.color = color;
             // Init board
             board = new int[8, 8];
             for (int i = 0; i < 8; i++)
@@ -22,23 +24,40 @@ namespace OthelloIA_G3
                     board[i, j] = -1;
                 }
             }
+
+            // Place first pions
             board[3, 3] = board[4, 4] = 0; // White
             board[4, 3] = board[3, 4] = 1; // Black
+        }
+
+        public Board(Board source)
+        {
+            color = source.color;
+
+            // Init board
+            board = new int[8, 8];
+
+            // Copy source values            
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    board[i, j] = source.board[i, j];
+                }
+            }
+
         }
 
         // IMPLEMENTS
         public int GetBlackScore()
         {
-            // throw new NotImplementedException();
             int score = 0;
             foreach (int i in board)
             {
                 if (i == 1)
-                {
                     score++;
-                }
             }
-            Console.WriteLine(score);
+            // Console.WriteLine(score);
             return score;
         }
 
@@ -55,6 +74,11 @@ namespace OthelloIA_G3
         public Tuple<int, int> GetNextMove(int[,] game, int level, bool whiteTurn)
         {
             // TODO Choisir le meilleur move avec un algo
+            int myColor = whiteTurn ? 0 : 1;
+
+            TreeNode root = new TreeNode(this, TreeNode.EType.MAX, myColor);
+
+            // AlphaBeta(root, level, minOrMax, parentValue);
             throw new NotImplementedException();
         }
 
@@ -64,11 +88,9 @@ namespace OthelloIA_G3
             foreach (int i in board)
             {
                 if (i == 0)
-                {
                     score++;
-                }
             }
-            Console.WriteLine(score);
+            // Console.WriteLine(score);
             return score;
         }
 
@@ -95,16 +117,16 @@ namespace OthelloIA_G3
 
             // Check voisin d'autre couleurs
             int colorOpponent;
-            int myColor;
+            int colorPlayer;
             if (isWhite == true)
             {
                 colorOpponent = 1;
-                myColor = 0;
+                colorPlayer = 0;
             }
             else
             {
                 colorOpponent = 0;
-                myColor = 1;
+                colorPlayer = 1;
             }
 
             //  (c; l) 
@@ -114,29 +136,10 @@ namespace OthelloIA_G3
             //  (-1; 1)  (0; 1)  (1; 1)
 
             // Init start/end for line and column
-            int cstart;
-            if (column <= 0)
-                cstart = 0;
-            else
-                cstart = -1;
-
-            int cend;
-            if (column >= 7)
-                cend = 0;
-            else
-                cend = 1;
-
-            int lstart;
-            if (line <= 0)
-                lstart = 0;
-            else
-                lstart = -1;
-
-            int lend;
-            if (line >= 7)
-                lend = 0;
-            else
-                lend = 1;
+            int cstart = column <= 0 ? 0 : -1;
+            int cend = column >= 7 ? 0 : 1;
+            int lstart = line <= 0 ? 0 : -1;
+            int lend = line >= 7 ? 0 : 1;
 
             //Console.WriteLine($"lstart : {lstart}\nlend : {lend}\ncstart : {cstart}\ncend : {cend}");
 
@@ -165,7 +168,7 @@ namespace OthelloIA_G3
                             while (column + copyC <= 7 && line + copyL <= 7 && column + copyC >= 0 && line + copyL >= 0)
                             {
                                 int currentCell = board[column + copyC, line + copyL];
-                                if (currentCell == myColor)
+                                if (currentCell == colorPlayer)
                                 {
                                     // Un pion a moi
                                     if (checkOnly)
@@ -177,7 +180,7 @@ namespace OthelloIA_G3
                                         playable = true;
                                         foreach (var tuple in opponentPion)
                                         {
-                                            board[tuple.Item1, tuple.Item2] = myColor;
+                                            board[tuple.Item1, tuple.Item2] = colorPlayer;
                                         }
                                     }
                                 }
@@ -203,7 +206,7 @@ namespace OthelloIA_G3
             }
             if (!checkOnly && playable)
             {
-                board[column, line] = myColor;
+                board[column, line] = colorPlayer;
                 return true;
             }
             return false;
---
+++ OthelloIA_G3/TreeNode.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OthelloIA_G3
+{
+    class TreeNode
+    {
+        public enum EType { MIN, MAX, LEAF };
+
+        // Attributs
+
+        public List<TreeNode> Children { get; set; }
+        public EType Type { get; set; }
+
+        private Board board;
+        private int myColor;
+
+        public TreeNode(Board board, EType type, int myColor)
+        {
+            this.board = board;
+            Type = type;
+            this.myColor = myColor;
+        }
+
+        public int Eval()
+        {
+            if (myColor == 0)
+            {
+                if (Type == EType.MAX)
+                    // On veut connaitre le meilleur coup des blancs si on est blanc
+                    return board.GetWhiteScore();
+                else
+                    return board.GetBlackScore();
+            }
+            else
+            {
+                if (Type == EType.MAX)
+                    return board.GetBlackScore();
+                else
+                    return board.GetWhiteScore();
+            }
+        }
+
+        public bool Final()
+        {
+            int cptPossibilite = 64;
+
+            for (int c = 0; c < 8; c++)
+            {
+                for (int l = 0; l < 8; l++)
+                {
+                    if (board.GetBoard()[c, l] > -1)
+                        // Cellule prise
+                        cptPossibilite--;
+                    else
+                    {
+                        if (board.IsPlayable(c, l, true) || board.IsPlayable(c, l, false))
+                            // Un des deux joueurs peut encore jouer
+                            return false;
+                        else
+                            // Personne peut jouer à cette case
+                            cptPossibilite--;
+                    }
+                }
+            }
+            // Aucune possibilité
+            return true;
+        }
+
+        public List<Tuple<int, int>> Ops(bool isWhite)
+        {
+            var ops = new List<Tuple<int, int>>();
+            for (int c = 0; c < 8; c++)
+            {
+                for (int l = 0; l < 8; l++)
+                {
+                    if (board.IsPlayable(c, l, isWhite))
+                    {
+                        ops.Add(new Tuple<int, int>(c, l));
+                    }
+                }
+            }
+            return ops;
+        }
+
+        public TreeNode Apply(int col, int line, bool isWhite)
+        {
+            Board newBoard = new Board(board);
+            newBoard.PlayMove(col, line, isWhite);
+
+            EType newType;
+            if (Type == EType.MAX)
+                newType = EType.MIN;
+            else
+                newType = EType.MAX;
+
+            return new TreeNode(newBoard, newType, myColor); ;
+        }
+    }
+}
