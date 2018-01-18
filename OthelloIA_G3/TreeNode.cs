0228351a0c07dd534f80f715bbb3e29afa7cf8ce
Pedro Costa <jules007.cesar@hotmail.com>
2018-01-18 17:07:51 +0100

    Ajout de la fonction d'évaluation

--- OthelloIA_G3/TreeNode.cs
+++ OthelloIA_G3/TreeNode.cs
@@ -28,10 +28,58 @@ namespace OthelloIA_G3
         /// <returns></returns>
         public double Eval()
         {
-            // TODO Refaire avec une matrice
-            int possibilityWhite = Ops(true).Count;
-            int possibilityBlack = Ops(false).Count;
-            return possibilityBlack - possibilityWhite;
+            int maxPlayerCoin, minPlayerCoin, maxPlayerMove, minPlayerMove, coinParity, mobility, corners;
+            maxPlayerCoin = minPlayerCoin = maxPlayerMove = minPlayerMove = coinParity = mobility = corners = 0;
+
+            int[,] mat_eval = new int[,] {{20, -10, 1, 1, 1, 1, -10, 20},
+                                          {-10, -7, 1, 1, 1, 1, -7, -10},
+                                              {1, 1, 1, 1, 1, 1, 1, 1},
+                                              {1, 1, 1, 1, 1, 1, 1, 1},
+                                              {1, 1, 1, 1, 1, 1, 1, 1},
+                                              {1, 1, 1, 1, 1, 1, 1, 1},
+                                          {-10, -7, 1, 1, 1, 1, -7, -10},
+                                          {20, -10, 1, 1, 1, 1, -10, 20}};
+
+            maxPlayerCoin = isWhite ? board.GetWhiteScore() : board.GetBlackScore();
+            minPlayerCoin = isWhite ? board.GetBlackScore() : board.GetWhiteScore();
+
+            if (maxPlayerCoin > minPlayerCoin){
+                coinParity = (100 * maxPlayerCoin)/(maxPlayerCoin + minPlayerCoin);
+            }else{
+                coinParity = -(100 * maxPlayerCoin) / (maxPlayerCoin + minPlayerCoin);
+            }
+
+            int nbrOfPlayableMoveWhite = Ops(isWhite).Count;
+            int nbrOfPlayableMoveBlack = Ops(isWhite).Count;
+
+            maxPlayerMove = isWhite ? nbrOfPlayableMoveWhite : nbrOfPlayableMoveBlack;
+            minPlayerMove = isWhite ? nbrOfPlayableMoveBlack : nbrOfPlayableMoveWhite;
+
+            if (maxPlayerMove > minPlayerMove){
+                mobility = (100 * maxPlayerMove) / (maxPlayerMove + minPlayerMove);
+            }else if (maxPlayerMove < minPlayerMove){
+                mobility = -(100 * maxPlayerMove) / (maxPlayerMove + minPlayerMove);
+            }
+
+            corners = GetCornerValue(mat_eval);
+            
+            return 2*coinParity + 5*mobility + 10*corners;
+        }
+
+        private int GetCornerValue(int[,] mat_eval)
+        {
+            int typeCoin = isWhite ? 0 : 1;
+            int cornerValue = 0;
+
+            for (int c = 0; c < 8; c++){
+                for (int l = 0; l < 8; l++){
+                    if (board.GetBoard()[c,l] == typeCoin){
+                        cornerValue += board.GetBoard()[c, l];
+                    }
+                }
+            }
+
+            return cornerValue;
         }
 
         public static bool Final(Board board)
